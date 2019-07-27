2e813e88715ad7ea9095d75e8f9fb050e1bab594
ParichayDidwania <parichaydidwania@gmail.com>
2019-07-27 23:20:10 +0530

    Added remaining files

---
+++ app/src/main/java/android/com/jeevika/AddressActivity.java
@@ -0,0 +1,25 @@
+package android.com.jeevika;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+
+    public void add_done(View view)
+    {
+       Intent i = new Intent(this,HomeActivity.class);
+       i.putExtra("mode",1);
+       startActivity(i);
+    }
+}
---
+++ app/src/main/java/android/com/jeevika/BookFragment.java
@@ -0,0 +1,180 @@
+package android.com.jeevika;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BookFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BookFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BookFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    ListView l;
+    ArrayList<String> a;
+
+    private OnFragmentInteractionListener mListener;
+
+    public BookFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BookFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BookFragment newInstance(String param1, String param2) {
+        BookFragment fragment = new BookFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        l = (ListView)getView().findViewById(R.id.book_list_view);
+        a = new ArrayList<String>();
+        a.add("Name");
+        a.add("Phone no");
+        a.add("Service");
+
+        CustomAdapter customAdapter = new CustomAdapter();
+
+        l.setAdapter(customAdapter);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return a.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.custom_layout,null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView3);
+
+            Bitmap src = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.deafult_image_icon2);
+
+            RoundedBitmapDrawable circularBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), src);
+            circularBitmapDrawable.setCornerRadius(360);
+            imageView.setImageDrawable(circularBitmapDrawable);
+
+            TextView name = (TextView) view.findViewById(R.id.textView4);
+            TextView phone = (TextView) view.findViewById(R.id.textView5);
+            TextView service = (TextView) view.findViewById(R.id.textView6);
+
+            name.setText(a.get(0));
+            phone.setText(a.get(1));
+            service.setText(a.get(2));
+
+            return view;
+        }
+    }
+}
